382e50b594509442170a0b27a4ee12970a1f613a
Veikka Niemi <veikka.niemi@outlook.com>
2025-03-03 10:21:51 +0200

    add yank highlight

--- nvim/.config/nvim/lua/akkiev/set.lua
+++ nvim/.config/nvim/lua/akkiev/set.lua
@@ -13,12 +13,23 @@ vim.opt.smartindent = true
 vim.opt.wrap = false
 
 vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 10
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
-vim.opt.colorcolumn = "80"
+--vim.opt.colorcolumn = "80"
+
+-- Highlight when yanking! (from kickstart)
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking text',
+    group = vim.api.nvim_create_augroup('highlight-yank', {clear = true}),
+    callback = function ()
+        vim.highlight.on_yank()
+    end,
+})
